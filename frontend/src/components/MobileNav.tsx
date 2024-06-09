a4090effe0693568f267938d9116f9bb104e022b
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-06-09 12:35:46 +0530

    ✨ Mobile navbar added.

---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,31 @@
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Menu } from "lucide-react";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          <span>Welcome to MealMate! </span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
