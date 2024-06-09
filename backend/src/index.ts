1f23bac9e8488dcf316967f38316cf89227da075
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-06-09 11:27:53 +0530

    🗃️ Database setup done successfully.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,29 @@
-import express ,{Request,Response}from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+// Connect with the database
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.log("Error connecting to the database", error);
+  });
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-app.get("/test",async(req:Request,res:Response)=>{
-    res.json({
-        message:"Hello World"
-    })
-})
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({
+    message: "Hello World",
+  });
+});
 
 // Listen
-app.listen(8080,()=>{
-    console.log(`The server is running on localhost:8080`)
-})
+app.listen(8080, () => {
+  console.log(`The server is running on localhost:8080`);
+});
