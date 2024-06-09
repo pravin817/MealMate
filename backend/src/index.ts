12b7ce1041875bd64918cb802b574e3f71105c96
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-06-09 10:36:35 +0530

    Backend setup done

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express ,{Request,Response}from "express";
+import cors from "cors";
+import "dotenv/config";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/test",async(req:Request,res:Response)=>{
+    res.json({
+        message:"Hello World"
+    })
+})
+
+// Listen
+app.listen(8080,()=>{
+    console.log(`The server is running on localhost:8080`)
+})
