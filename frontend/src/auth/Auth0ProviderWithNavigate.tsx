805bbac1d660c5909086dab165956682ac6f61a6
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-06-10 14:19:00 +0530

    ✨ Auth0 access token sends to the backend request

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,15 +1,17 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
+// import { useCreateMyUser } from "@/api/MyUserApi";
 import { Auth0Provider, AppState, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const { createUser, isError, isLoading, isSuccess } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENTID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+  const navigate = useNavigate();
 
   if (!domain || !clientId || !redirectUri) {
     throw new Error(
@@ -18,12 +20,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({
-        auth0Id: user?.sub,
-        email: user?.email,
-      });
-    }
+    navigate("/auth-callback");
   };
   return (
     <Auth0Provider
