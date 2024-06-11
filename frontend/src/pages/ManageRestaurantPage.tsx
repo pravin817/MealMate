084833d4236149ae56c1f03212a27b996680f53e
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-06-11 23:57:07 +0530

    ✨ Add restaurant form page added

---
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,14 @@
+import { Separator } from "@/components/ui/separator";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  return (
+    <div>
+      <ManageRestaurantForm />
+      <Separator />
+      
+    </div>
+  );
+};
+
+export default ManageRestaurantPage;
